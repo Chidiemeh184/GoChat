312194ce01f906de5795dd500fdce29166beffea
Chidi Emeh <chidiemeh184@gmail.com>
2018-05-16 21:30:43 -0400

    Log In Screen

--- GoChat/VC/LogInViewController.swift
+++ GoChat/VC/LogInViewController.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    
+    @IBOutlet weak var anonymousButton: UIButton!
+    @IBOutlet weak var googleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        anonymousButton.layer.borderWidth = 2.0
+        anonymousButton.layer.borderColor = UIColor.black.cgColor
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
+    @IBAction func logInAnonymouslyTapped(_ sender: Any) {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func googleButtonTapped(_ sender: Any) {
     }
-    */
-
+    
 }
